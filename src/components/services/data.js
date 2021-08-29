b3fa39b5e8611b5f9e99ba2fba4ddb9942909a04
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-08-28 21:40:07 -0400

    add the project arc developemnt

--- src/components/services/data.js
+++ src/components/services/data.js
@@ -4,9 +4,17 @@ import Porject3 from '../../images/project3.png'
 import Porject4 from '../../images/project4.png'
 import Porject7 from '../../images/project7.png'
 import Porject6 from '../../images/project6.png'
+import Project8 from '../../images/project8.png'
 
 
 export const Project1 = [
+  {
+    title: 'Arc development',
+    skill: 'React, Material UI',
+    img: Project8,
+    url: 'https://rudwo8134.github.io/ArcDevelopment/',
+    type: 'react backend',
+  },
   {
     title: 'UBF church',
     skill: 'React, firebase',
